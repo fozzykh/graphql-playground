b572d01ee4af844953e7eb17b776898d7b9a78fd
Fozy Khoury <fozy.khoury@fineway.de>
2018-09-21 16:40:40 +0200

    wrapping app with apollo provider

--- src/App.js
+++ src/App.js
@@ -1,51 +1,50 @@
 import React, { Component } from 'react';
-import ApolloClient from 'apollo-boost';
 import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 import Movie from './components/Movie';
 
 import './App.css';
 
 class App extends Component {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      movies: []
-    }
-  }
+  renderLoading = () => (
+    <div className="loadingContainer">
+      <p className="loadingText"> Fetching Movies .... </p>
+    </div>
+  );
 
-  componentDidMount() {
-    const graphQlClient = new ApolloClient({
-      uri: 'https://movie-database-graphql-qwqnwstigc.now.sh/graphql'
-    });
-
-    graphQlClient.query({
-      query: gql`
-        {
-          movies(query: "id:1") {
-            title
-            id
-            poster_path
-          }
-        }
-      `
-    }).then(result => this.setState({ movies: result.data.movies}));
-  }
+  renderError = () => <p> Error fetching Movies ...</p>;
 
   render() {
-    const { movies } = this.state;
     return (
-      <div className="appContainer">
-        {
-          movies.map(movie =>
-            <Movie
-              key={movie.id}
-              title={movie.title}
-              image={movie.poster_path}
-            />
-          )
-        }
-      </div>
+      <Query
+        query={gql`
+          {
+            movies(query: "id:1") {
+              title
+              id
+              poster_path
+            }
+          }
+        `}
+      >
+        {({ loading, error, data }) => (
+          loading ? this.renderLoading() :
+          error ? this.renderError() :
+
+          <div className="appContainer">
+            {
+              data.movies.map(movie =>
+                <Movie
+                  key={movie.id}
+                  title={movie.title}
+                  image={movie.poster_path}
+                />
+              )
+            }
+          </div>
+        )}
+      </Query>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const graphQlClient = new ApolloClient({
+  uri: 'https://movie-database-graphql-qwqnwstigc.now.sh/graphql'
+});
+
+const Root = () => (
+  <ApolloProvider client={graphQlClient} >
+    <App />
+  </ApolloProvider>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
